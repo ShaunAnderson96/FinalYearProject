845d19170694b302493285e11c07da919bbb458a
Shaun Anderson <shaunanderson2008@hotmail.com>
2018-02-13 16:51:23 +0000

    Code added

---
+++ app.js
@@ -0,0 +1,59 @@
+//bringing in all dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');//when submitting a form it will grab the data
+const cors = require('cors'); 
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect to database
+mongoose.connect(config.database);
+
+//on connection alert me
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database' + config.database);
+});
+
+//on error alert me
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to database' + err);
+});
+
+//initialise app variable with express
+const app = express();
+
+//gets the users file in the routes folder
+const users = require('./routes/users');
+
+//port number into port variable
+const port = 3000;
+
+//allows to make request to API from different domain name
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'front-end')));
+
+//bodyParser 
+app.use(bodyParser.json());
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+
+//allows the use of users in the route folder
+app.use('/users', users);
+
+//route to homepage
+app.get('/', (req, res) => {
+    res.send('Invaild Endpoint');
+})
+
+// starts the server
+app.listen(port, () => {
+    console.log('Server started on port '+port);
+});
---
+++ routes/users.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../config/database');
+
+
+
+//Register route
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user)=>{
+        if(err){
+            res.json({success: false, msg:'Failed to register user'});
+        }else{
+            res.json({success: true, msg:'User Registered'});
+        }
+        
+    });
+});
+
+//Authenicate
+router.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check for username
+    User.getUserByUsername(username, (err, user) => {
+        if(err) throw err;
+        if(!user){
+            return res.json({success: false, msg : 'User not found'});
+        }
+
+        //match password if username exists
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if(err) throw err;
+            if(isMatch){
+                const token = jwt.sign(user.toJSON(), config.secret, {
+                    expiresIn: 604800
+
+                });
+                
+                //send this responds if pword and username matches
+                res.json({
+                    success: true,
+                    token: 'JWT is '+ token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                        }
+                });
+
+            }
+            else {
+                //if username and pword doesnt match send this response
+                return res.json({success: false, msg : 'Wrong password'});
+            }
+        });
+    
+    });
+});
+
+//Profile
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+
+module.exports = router;
